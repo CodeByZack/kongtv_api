1643f47e8d682f60a1f01c5f2689689aaa18c1a3
dongkai <958059970@qq.com>
2021-08-05 19:37:18 +0800

    更新utils

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,16 @@
-const axios = require('axios');
-const temme = require('temme').default;
-const AIQIYI_DY = 'https://top.baidu.com/board?tab=movie';
-const AIQIYI_DSJ = 'https://top.baidu.com/board?tab=teleplay';
-const AIQIYI_DM= 'https://top.baidu.com/board?tab=cartoon';
-const AIQIYI_ZY = 'https://top.baidu.com/board?tab=variety';
+const axios = require("axios");
+const temme = require("temme").default;
+
+const AIQIYI_DY = "https://top.baidu.com/board?tab=movie";
+const AIQIYI_DSJ = "https://top.baidu.com/board?tab=teleplay";
+const AIQIYI_DM = "https://top.baidu.com/board?tab=cartoon";
+const AIQIYI_ZY = "https://top.baidu.com/board?tab=variety";
 
 const URL_MAP = {
-    dy : AIQIYI_DY,
-    dsj : AIQIYI_DSJ,
-    dm : AIQIYI_DM,
-    zy :AIQIYI_ZY
+  dy: AIQIYI_DY,
+  dsj: AIQIYI_DSJ,
+  dm: AIQIYI_DM,
+  zy: AIQIYI_ZY,
 };
 
 const rule = `.category-wrap_iQLoo @data {
@@ -17,34 +18,58 @@ const rule = `.category-wrap_iQLoo @data {
     .main-title .sub-des@actors{}
 }`;
 
-const needKeys = ['vod_id','type_id','vod_time','vod_level','type_id_1','vod_actor','vod_area','vod_blurb','vod_class','vod_content','vod_director','vod_lang','vod_name','vod_pic','vod_play_url','vod_remarks','vod_year'];
+const needKeys = [
+  "vod_id",
+  "type_id",
+  "vod_time",
+  "vod_level",
+  "type_id_1",
+  "vod_actor",
+  "vod_area",
+  "vod_blurb",
+  "vod_class",
+  "vod_content",
+  "vod_director",
+  "vod_lang",
+  "vod_name",
+  "vod_pic",
+  "vod_play_url",
+  "vod_remarks",
+  "vod_year",
+];
 
-const needKeysStr = needKeys.join(',');
+const needKeysStr = needKeys.join(",");
 
-const filterKeys = (obj)=>{
+const filterKeys = (obj) => {
   const newObj = {};
-  needKeys.forEach(key=>{newObj[key] = obj[key]});
+  needKeys.forEach((key) => {
+    newObj[key] = obj[key];
+  });
   return newObj;
 };
 
-
-const getTopAdvice = async (type)=>{    
-    const baseUrl = URL_MAP[type];
-    if(!baseUrl) return null;
-    try {
-        const res = await axios.get(baseUrl);
-        const html = res.data;
-        const items = temme(html,rule);
-        const data = items.data.filter(_=>_ && _.name);
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
-    return null;
+const getTopAdvice = async (type) => {
+  const baseUrl = URL_MAP[type];
+  if (!baseUrl) return null;
+  try {
+    const res = await axios.get(baseUrl, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+      },
+    });
+    const html = res.data;
+    const items = temme(html, rule);
+    const data = items.data.filter((_) => _ && _.name);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 };
 
 module.exports = {
   filterKeys,
   needKeysStr,
-  getTopAdvice
-}
+  getTopAdvice,
+};
