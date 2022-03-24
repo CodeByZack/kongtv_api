e3869703a49b32caba049ac54f3b833559085e5f
dongkai <958059970@qq.com>
2022-03-24 15:49:04 +0800

    修复推荐源为豆瓣

--- src/model/movie.js
+++ src/model/movie.js
@@ -1,15 +1,15 @@
-const { think } = require("thinkjs");
-const { filterKeys, needKeysStr, getTopAdvice } = require("../utils");
+const { think } = require('thinkjs');
+const { needKeysStr, getTopAdvice } = require('../utils');
 
 const categoryKeys = {
-  dy : {type_id_1:1},
-  dsj : {type_id_1:2},
-  zy : {type_id:3},
-  dm : {type_id:4},
-  ll : {type_id:17}
+  dy: { type_id_1: 1 },
+  dsj: { type_id_1: 2 },
+  zy: { type_id: 3 },
+  dm: { type_id: 4 },
+  ll: { type_id: 17 }
 };
 
-const indexFlag = { vod_level : 1 };
+const indexFlag = { vod_level: 1 };
 
 module.exports = class extends think.Model {
   get tableName() {
@@ -20,104 +20,124 @@ module.exports = class extends think.Model {
     return 'vod_id';
   }
 
-  async index(){
+  async index() {
+    const dyWhere = { ...categoryKeys.dy, ...indexFlag };
+    const dsjWhere = { ...categoryKeys.dsj, ...indexFlag };
+    const zyWhere = { ...categoryKeys.zy, ...indexFlag };
+    const dmWhere = { ...categoryKeys.dm, ...indexFlag };
 
-    const dyWhere = {...categoryKeys.dy,...indexFlag};
-    const dsjWhere = {...categoryKeys.dsj,...indexFlag};
-    const zyWhere = {...categoryKeys.zy,...indexFlag};
-    const dmWhere = {...categoryKeys.dm,...indexFlag};
-
-    const dy = await this.field(needKeysStr).order(['vod_time DESC']).limit(6).where(dyWhere).cache('index_dy').select();
-    const dsj = await this.field(needKeysStr).order(['vod_time DESC']).limit(6).where(dsjWhere).cache('index_dsj').select();
-    const zy = await this.field(needKeysStr).order(['vod_time DESC']).limit(6).where(zyWhere).cache('index_zy').select();
-    const dm = await this.field(needKeysStr).order(['vod_time DESC']).limit(6).where(dmWhere).cache('index_dm').select();
-    return [...dy,...dsj,...zy,...dm];
+    const dy = await this.field(needKeysStr)
+      .order(['vod_time DESC'])
+      .limit(6)
+      .where(dyWhere)
+      .cache('index_dy')
+      .select();
+    const dsj = await this.field(needKeysStr)
+      .order(['vod_time DESC'])
+      .limit(6)
+      .where(dsjWhere)
+      .cache('index_dsj')
+      .select();
+    const zy = await this.field(needKeysStr)
+      .order(['vod_time DESC'])
+      .limit(6)
+      .where(zyWhere)
+      .cache('index_zy')
+      .select();
+    const dm = await this.field(needKeysStr)
+      .order(['vod_time DESC'])
+      .limit(6)
+      .where(dmWhere)
+      .cache('index_dm')
+      .select();
+    return [...dy, ...dsj, ...zy, ...dm];
   }
 
-  async pageByCategory(options){
+  async pageByCategory(options) {
+    const { type, page = 1, pageSize = 12, area, year, _class } = options;
 
-    const {
-      type,
-      page = 1,
-      pageSize = 12,
-      area,
-      year,
-      _class
-    } = options;
-
-
-    let where = {...categoryKeys[type]};
-    if( area ){
+    const where = { ...categoryKeys[type] };
+    if (area) {
       where.vod_area = ['like', area];
     }
-    if( year ){
+    if (year) {
       where.vod_year = ['like', year];
     }
-    if( _class ){
+    if (_class) {
       where.vod_class = ['like', _class];
     }
 
     const cacheKey = `category_${type}_${page}_${pageSize}_${area}_${year}_${_class}`;
 
-    const res = await this.field(needKeysStr).where(where).page(page,pageSize).order(['vod_time DESC']).cache(cacheKey).select();
-    
+    const res = await this.field(needKeysStr)
+      .where(where)
+      .page(page, pageSize)
+      .order(['vod_time DESC'])
+      .cache(cacheKey)
+      .select();
+
     return res;
   }
 
-  async search(word,page=1,pageSize=10){
+  async search(word, page = 1, pageSize = 10) {
     const cacheKey = `search_${word}`;
-    const res = await this.field(needKeysStr).where({ vod_name : ['like',`%${word}%`] }).order(['vod_time DESC']).page(page,pageSize).cache(cacheKey).select();
+    const res = await this.field(needKeysStr)
+      .where({ vod_name: ['like', `%${word}%`] })
+      .order(['vod_time DESC'])
+      .page(page, pageSize)
+      .cache(cacheKey)
+      .select();
     return res;
   }
 
-  async updateIndex(type){
-
+  async updateIndex(type) {
     const where = categoryKeys[type];
-    if(!where)return;
+    if (!where) return;
 
-    const condition = { ...where,vod_level:1 };
+    const condition = { ...where, vod_level: 1 };
 
-
-    const innerFn = async ()=>{
-
-      //拿到当前最新的数据
+    const innerFn = async() => {
+      // 拿到当前最新的数据
       const topAdviceArr = await getTopAdvice(type);
 
-      //找到之前推荐的
+      // 找到之前推荐的
       const oldAdvice = await this.field(['vod_id']).where(condition).select();
 
-      //reset vod_level
-      const resetArr = oldAdvice.map(item=>({...item,vod_level:0}));
+      // reset vod_level
+      const resetArr = oldAdvice.map((item) => ({ ...item, vod_level: 0 }));
 
-      //重置
-      const res = await this.updateMany(resetArr);
+      // 重置
+      await this.updateMany(resetArr);
 
       let count = 0;
-      let newAdviceArr = [];
+      const newAdviceArr = [];
       for (const item of topAdviceArr) {
-        const whereLike = { 
+        const whereLike = {
           ...where,
-          vod_name:['like',`%${item.name}%`]
+          vod_name: ['like', `%${item.name}%`]
         };
 
-        // if(item.actors && item.actors.length){
-        //   whereLike.vod_actor = ['like', item.actors.map(actor=>`%${actor}%`)];
-        // }
+        if (item.actors && item.actors.length) {
+          whereLike.vod_actor = [
+            'like',
+            item.actors.map((actor) => `%${actor}%`)
+          ];
+        }
 
-        if(count<6){
+        if (count < 6) {
           const findObj = await this.where(whereLike).find();
-          if(think.isEmpty(findObj))continue;
-          const affectsRows = await this.where({vod_id:findObj.vod_id}).update({vod_level:1});
-          if(affectsRows > 0) {
+          if (think.isEmpty(findObj)) continue;
+          const affectsRows = await this.where({
+            vod_id: findObj.vod_id
+          }).update({ vod_level: 1 });
+          if (affectsRows > 0) {
             count++;
             newAdviceArr.push(findObj);
-          };
+          }
         }
       }
-      // console.log(newAdviceArr.map(_=>_.vod_name));
+      console.log(newAdviceArr.map(_=>_.vod_name));
     };
     return this.transaction(innerFn);
-
   }
-
 };
--- src/utils.js
+++ src/utils.js
@@ -1,44 +1,42 @@
-const axios = require("axios");
-const temme = require("temme").default;
+const axios = require('axios');
 
-const AIQIYI_DY = "https://top.baidu.com/board?tab=movie";
-const AIQIYI_DSJ = "https://top.baidu.com/board?tab=teleplay";
-const AIQIYI_DM = "https://top.baidu.com/board?tab=cartoon";
-const AIQIYI_ZY = "https://top.baidu.com/board?tab=variety";
+const DOUBAN_DY =
+  'https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=%E7%94%B5%E5%BD%B1';
+const DOUBAN_DSJ =
+  'https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=%E7%94%B5%E8%A7%86%E5%89%A7';
+const DOUBAN_DM =
+  'https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=%E5%8A%A8%E6%BC%AB';
+const DOUBAN_ZY =
+  'https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=%E7%BB%BC%E8%89%BA';
 
 const URL_MAP = {
-  dy: AIQIYI_DY,
-  dsj: AIQIYI_DSJ,
-  dm: AIQIYI_DM,
-  zy: AIQIYI_ZY,
+  dy: DOUBAN_DY,
+  dsj: DOUBAN_DSJ,
+  dm: DOUBAN_DM,
+  zy: DOUBAN_ZY
 };
 
-const rule = `.category-wrap_iQLoo @data {
-    .c-single-text-ellipsis{$name|replace(/\s*/g,"")}
-    .main-title .sub-des@actors{}
-}`;
-
 const needKeys = [
-  "vod_id",
-  "type_id",
-  "vod_time",
-  "vod_level",
-  "type_id_1",
-  "vod_actor",
-  "vod_area",
-  "vod_blurb",
-  "vod_class",
-  "vod_content",
-  "vod_director",
-  "vod_lang",
-  "vod_name",
-  "vod_pic",
-  "vod_play_url",
-  "vod_remarks",
-  "vod_year",
+  'vod_id',
+  'type_id',
+  'vod_time',
+  'vod_level',
+  'type_id_1',
+  'vod_actor',
+  'vod_area',
+  'vod_blurb',
+  'vod_class',
+  'vod_content',
+  'vod_director',
+  'vod_lang',
+  'vod_name',
+  'vod_pic',
+  'vod_play_url',
+  'vod_remarks',
+  'vod_year'
 ];
 
-const needKeysStr = needKeys.join(",");
+const needKeysStr = needKeys.join(',');
 
 const filterKeys = (obj) => {
   const newObj = {};
@@ -48,21 +46,16 @@ const filterKeys = (obj) => {
   return newObj;
 };
 
-const getTopAdvice = async (type) => {
+const getTopAdvice = async(type) => {
   const baseUrl = URL_MAP[type];
   if (!baseUrl) return null;
   try {
-    const res = await axios.get(baseUrl, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
-      },
-    });
-    const html = res.data;
-    const items = temme(html, rule);
-    const data = items.data.filter((_) => _ && _.name).map(_=>({ name : _.name.replace(/\s*/g,'') , actors : [] }));
+    const res = await axios.get(baseUrl);
+    const html = res.data.data;
+    const data = html.map((i) => ({ name: i.title, actors: i.casts }));
     return data;
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.log(error);
   }
   return null;
@@ -71,5 +64,5 @@ const getTopAdvice = async (type) => {
 module.exports = {
   filterKeys,
   needKeysStr,
-  getTopAdvice,
+  getTopAdvice
 };
